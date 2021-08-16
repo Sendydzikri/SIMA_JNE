ac9314e4fe86f09f4589f2c0c73e2e0316ee4d6e
Sendydzikri <dzikrisendy6@gmail.com>
2021-08-16 11:57:51 +0700

    .

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+
+$route['kelola_user'] = 'admin/kelola_user/c_dashboard/index';
+$route['kelola_data_user'] = 'admin/kelola_user/c_data_user/index';
+$route['kelola_data_user/tambah'] = 'admin/kelola_user/c_data_user/tambah';
